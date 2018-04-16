3624868d2064a6bf0b0d4857a300c36a52e17e3c
Eric deRegt <ericderegt@gmail.com>
2018-04-16 11:34:03 -0400

    fix format string

--- models.py
+++ models.py
@@ -246,7 +246,8 @@ class Order(db.Model):
     def init_db(app):
         """ Initializes the database session """
         Order.logger.info('Initializing database')
-        Order.logger.info('With URL {}').format(app.config['SQLALCHEMY_DATABASE_URI'])
+        url = 'With URL {}'.format(app.config['SQLALCHEMY_DATABASE_URI'])
+        Order.logger.info(url)
         Order.app = app
         # This is where we initialize SQLAlchemy from the Flask app
         db.init_app(app)
